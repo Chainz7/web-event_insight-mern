6a819b7e4eeec9bf84ffe9863755abac907b237f
Chainz <61576690+Chainz7@users.noreply.github.com>
2023-10-10 09:13:10 +0700

    Find Events
    Frontend

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer";
 import CustomButton from "./CustomButton";
 import TextInput from "./TextInput";
 import SignUp from "./SignUp";
+import Header from "./Header";
 
-export { Navbar, Footer, CustomButton, TextInput, SignUp };
+export { Navbar, Footer, CustomButton, TextInput, SignUp, Header };
--- client/src/pages/FindJobs.jsx
+++ client/src/pages/FindJobs.jsx
@@ -1,7 +1,45 @@
+// @ts-nocheck
 import React from "react";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { BiBriefcaseAlt2 } from "react-icons/bi";
+import { BsStars } from "react-icons/bs";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+
+import Header from "../components/Header";
+import { experience, jobTypes, jobs } from "../utils/data";
+import { CustomButton, JobCard, ListBox } from "../components";
 
 const FindJobs = () => {
-  return <div>FindJobs</div>;
+  const [sort, setSort] = useState("Newest");
+  const [page, setPage] = useState(1);
+  const [numPage, setNumPage] = useState(1);
+  const [recordCount, setRecordCount] = useState(0);
+  const [data, setData] = useState([]);
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [jobLocation, setJobLocation] = useState("");
+  const [filterJobTypes, setFilterJobTypes] = useState([]);
+  const [filterExp, setFilterExp] = useState([]);
+
+  const [isFetching, setIsFetching] = useState(false);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Header
+        title="Find Evemt with Ease"
+        type="home"
+        handleClick={() => {}}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        location={jobLocation}
+        setLocation={setJobLocation}
+      />
+    </div>
+  );
 };
 
 export default FindJobs;
