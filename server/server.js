9f399033032b5352db4725b0086f5bc99d3720bf
Chainz <61576690+Chainz7@users.noreply.github.com>
2023-10-10 17:53:33 +0700

    MongoDB Connection
    Backend

--- server/server.js
+++ server/server.js
@@ -6,10 +6,7 @@ import bodyParser from "body-parser";
 
 import xss from "xss-clean";
 import mongoSanitize from "express-mongo-sanitize";
-
 import dbConnection from "./dbConfig/dbConnection.js";
-import router from "./routes/index.js";
-import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
 
@@ -18,3 +15,19 @@ const app = express();
 const PORT = process.env.PORT || 8800;
 
 // MONGODB CONNECTION
+dbConnection();
+
+// middlenames
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+app.listen(PORT, () => {
+  console.log(`Dev Server running on port: ${PORT}`);
+});
